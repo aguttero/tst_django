8d449752559f4a853e6816bb0f26ee8aae9e6b76
aguttero <aguttero@gmail.com>
2025-11-17 16:44:24 -0300

    v02 css js test home about pages

--- myproject/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def homepage(request):
-    return HttpResponse("Hello World first django page")
-
-def about(request):
-    return HttpResponse("This is the about page")
